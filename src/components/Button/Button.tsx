d406f6371baf9309a53335393d2b41a2d3a11b08
gabriel.miranda <gabriel.miranda@hpp.org.br>
2023-08-24 17:10:13 -0300

    style: estilização da pagina de about

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './button.module.css'
+import Link from 'next/link'
+
+interface ButtonProps {
+  text: string
+  url: string
+}
+
+export const Button = ({text, url}: ButtonProps) => {
+  return (
+    <Link href={url}>
+      <button className={styles.container}>{text}</button>
+    </Link>
+  )
+}
