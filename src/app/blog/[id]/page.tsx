dcb408767db55af19fbbb7200b40652549417324
gabriel.miranda <gabriel.miranda@hpp.org.br>
2024-04-01 15:15:07 -0300

    feat: adiciona runtime edge nas paginas de api

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import React from 'react'
 import styles from "./page.module.css";
 import Image from 'next/image';
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import React from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
